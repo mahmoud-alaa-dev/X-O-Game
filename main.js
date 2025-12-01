d3d583f5e969231c731155339dd3de5da658f780
Mahmoud Alaa <ma7moud.3la2.2ldeen@gmail.com>
2025-12-02 00:02:02 +0200

    update

--- main.js
+++ main.js
@@ -4,44 +4,46 @@ const squares = document.querySelectorAll(".squares div");
 function checkWin(){
 
         if(squares[0].innerHTML == squares[1].innerHTML && squares[1].innerHTML == squares[2].innerHTML && squares[0].innerHTML != ""){
-            endGame(squares[0], squares[1], squares[2])
+            endGame(squares[0], squares[1], squares[2]);
             finish = true;
         }
         else if(squares[3].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[5].innerHTML && squares[3].innerHTML != ""){
-            endGame(squares[3], squares[4], squares[5])
+            endGame(squares[3], squares[4], squares[5]);
             finish = true;
         }
         else if(squares[6].innerHTML == squares[7].innerHTML && squares[7].innerHTML == squares[8].innerHTML && squares[6].innerHTML != ""){
-            endGame(squares[6], squares[7], squares[8])
+            endGame(squares[6], squares[7], squares[8]);
             finish = true;
         }
         else if(squares[0].innerHTML == squares[3].innerHTML && squares[3].innerHTML == squares[6].innerHTML && squares[0].innerHTML != ""){
-            endGame(squares[0], squares[3], squares[6])
+            endGame(squares[0], squares[3], squares[6]);
             finish = true;
         }
         else if(squares[1].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[7].innerHTML && squares[1].innerHTML != ""){
-            endGame(squares[1], squares[4], squares[7])
+            endGame(squares[1], squares[4], squares[7]);
             finish = true;
         }
         else if(squares[2].innerHTML == squares[5].innerHTML && squares[5].innerHTML == squares[8].innerHTML && squares[2].innerHTML != ""){
-            endGame(squares[2], squares[5], squares[8])
+            endGame(squares[2], squares[5], squares[8]);
             finish = true;
         }
         else if(squares[0].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[8].innerHTML && squares[0].innerHTML != ""){
-            endGame(squares[0], squares[4], squares[8])
+            endGame(squares[0], squares[4], squares[8]);
             finish = true;
         }
         else if(squares[2].innerHTML == squares[4].innerHTML && squares[4].innerHTML == squares[6].innerHTML && squares[2].innerHTML != ""){
-            endGame(squares[2], squares[4], squares[6])
+            endGame(squares[2], squares[4], squares[6]);
             finish = true;
         }
         else{
             checkDraw();
-        }
-    }
+            
+        };
+    };
 
 function checkDraw(){
     if([...squares].every(box => box.innerHTML !== "")){
+        finish = true;
         title.innerHTML = "Draw!";
         let dotRepeat = setInterval(() => {
             title.innerHTML += ".";
@@ -50,9 +52,8 @@ function checkDraw(){
             resetGame();
             clearInterval(dotRepeat)
         }, 4000);
-    }
-
-}
+    };
+};
 
 function endGame(box1, box2, box3){
     box1.style.transition = "0.6s";
@@ -88,7 +89,7 @@ function endGame(box1, box2, box3){
         resetGame();
     }, 4000)
 
-}
+};
 
 function resetGame(){
     squares.forEach(box => {
@@ -98,11 +99,12 @@ function resetGame(){
     });
     turn = "X";
     title.innerHTML = "X-O Game";
-}
+};
 
 
 let turn = "X";
 let finish = false;
+
 function gamePlay(id){
 
     let box = document.getElementById(id);
@@ -118,33 +120,31 @@ function gamePlay(id){
             finish = false;
             return;
         };
-    }
+    };
     if(playTurn){
         setTimeout(() => {
         let best = getBestMove("O");
         let block = getBestMove("X");
         if(best !== -1){
-            squares[best].innerHTML = "O";
+            squares[best].innerHTML = turn;
         }else if(block !== -1){
-            squares[block].innerHTML = "O";
+            squares[block].innerHTML = turn;
         }else if(squares[4].innerHTML === ""){
-            squares[4].innerHTML = "O";
+            squares[4].innerHTML = turn;
         }else{
             const empty = [...squares].filter(b => b.innerHTML == "");
             if(empty.length > 0){
-                empty[Math.floor(Math.random() * empty.length)].innerHTML = "O";
+                empty[Math.floor(Math.random() * empty.length)].innerHTML = turn;
             }
-        }
+        };
         turn = "X";
         title.innerHTML = turn;
         squares.forEach(div => div.style.pointerEvents = "auto");
         playTurn = false;
         checkWin();
         if(finish) finish = false;
-    
-        
-}, 1000);
-        }
+        }, 1000);
+    };
 
 };
 
@@ -155,10 +155,10 @@ function getBestMove(player){
         if(isWinner(player)){
             squares[i].innerHTML = "";
             return i;
-        }
+        };
         squares[i].innerHTML = "";
-        }
-    }
+        };
+    };
     return -1;
 };
 
@@ -172,7 +172,7 @@ function isWinner(player){
         return squares[a].innerHTML == player &&
         squares[b].innerHTML == player &&
         squares[c].innerHTML == player
-    })
+    });
 };
 
 squares.forEach(box => {
